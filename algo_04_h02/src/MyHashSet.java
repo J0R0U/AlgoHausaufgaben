1f6af4f51171b223b8df6e4fc9be570fe4274aa5
Jonas Rouven Schönauer <jonas.schoenauer@inform-software.com>
2019-04-02 15:27:04 +0200

    Commented Homework 02 plz check again for ä,ö,ü and ß.
    UTF-8 4 da win :blossom:

--- algo_04_h02/src/MyHashSet.java
+++ algo_04_h02/src/MyHashSet.java
@@ -9,51 +9,50 @@ import java.util.ArrayList;
  *        soll.
  */
 public class MyHashSet<K> {
-	private ArrayList<K>[] data;
-	private int elements;
-	private int lists;
+	private static final int INITIAL_SIZE = 10; // Die initiale Groesse der HashTabelle
+	private static final int THRESHHOLD   =  2; // Der Wert ab welchem die HashTabelle vergrößert werden soll
+	private static final int FACTOR       =  2; // Der Factor um den die HashTabelle erweitert werden soll
+
+	private ArrayList<K>[] data; // Die HashTabelle
+	private int elements;        // Die Anzahl der Elemente
+	
+	/**
+	 * In den Methoden @code{add(K element)} und @code{delete(K element)} wurde bewusst
+	 * auf die Verwendung der Methode contains verzichtet, da das Berechnen einen
+	 * HashWerts eventuell ein schlechtes Laufzeitverhalten hat.
+	 */
 
 	/**
-	 * 
+	 * Der Konstruktor fuer Objekte der Klasse MyHashSet.
 	 */
-	@SuppressWarnings("unchecked")
 	public MyHashSet() {
-		data = (ArrayList<K>[]) new ArrayList[10];
+		data     = createArray(INITIAL_SIZE);
 		elements = 0;
-		lists = 0;
 	}
 
 	/**
-	 * 
-	 * @param element
-	 * @return
+	 * Diese Methode fuegt dem Set ein weiteres Element hinzu. Sollte die Anzahl der Elemente geteilt
+	 * durch die Anzahl der Listen größer als 2 sein, wird das Set dynamisch erweitert. 
+	 * @param element Das Element welches hinzugefuegt werden soll.
+	 * @return {@code true} Wenn das Element bereits existriert, sonst {@code false}
 	 */
 	public boolean add(K element) {
 		if(addElement(element, data)) {
 			return true;
 		}
 
-		if(elements * 1. / lists > 2) {
-			@SuppressWarnings("unchecked")
-			ArrayList<K>[] temp = (ArrayList<K>[]) new ArrayList[data.length * 2];
-
-			lists = 0;
-			elements = 0;
-
-			for(K i : getElements()) {
-				addElement(i, temp);
-			}
-
-			data = temp;
+		// Prüfen ob erweitert werden muss
+		if((double) elements / data.length > THRESHHOLD) {
+			doubleSize();
 		}
 
 		return false;
 	}
 
 	/**
-	 * 
-	 * @param element
-	 * @return
+	 * Diese Methode loescht ein Element welches in dem HashSet gespeichert wird.
+	 * @param element Das Element welches geloescht werden soll.
+	 * @return {@code true} Wenn das Element existrierte, sonst {@code false}
 	 */
 	public boolean delete(K element) {
 		int pos = hash(element, data.length);
@@ -68,9 +67,9 @@ public class MyHashSet<K> {
 	}
 
 	/**
-	 * 
-	 * @param element
-	 * @return
+	 * Diese Methode prueft ob ein Element in dem aktuellen HashSet vorhanden ist.
+	 * @param element Das Element für welches geprüft werden soll ob es bereits vorhanden ist.
+	 * @return {@code true} Wenn das Element existriert, sonst {@code false}
 	 */
 	public boolean contains(K element) {
 		int pos = hash(element, data.length);
@@ -83,8 +82,8 @@ public class MyHashSet<K> {
 	}
 
 	/**
-	 * 
-	 * @return
+	 * Diese Methode gibt alle gespeicherten Elemente dieses HashSets in einer ArrayListe zurueck.
+	 * @return Eine {@code ArrayList<K>} die alle Elemente enthaelt.
 	 */
 	public ArrayList<K> getElements() {
 		ArrayList<K> ret = new ArrayList<>();
@@ -99,35 +98,63 @@ public class MyHashSet<K> {
 	}
 
 	/**
-	 * 
-	 * @param element
-	 * @param hashMap
-	 * @return
+	 * Diese Methode erwitert die Groesse der HashTabelle indem ein neues Feld angelegt wird.
+	 * Es müssen alle Elemente erneut eingefuegt werden.
 	 */
-	private boolean addElement(K element, ArrayList<K>[] hashMap) {
-		int pos = hash(element, hashMap.length);
+	private void doubleSize() {
+		ArrayList<K>[] temp = createArray(data.length * FACTOR);
 
-		if(hashMap[pos] == null) {
-			hashMap[pos] = new ArrayList<>();
-			lists++;
+		elements = 0;
+		
+		for(int i = 0; i < data.length; i++) {
+			for(K element : data[i]) {
+				addElement(element, temp);
+			}
 		}
 
-		if(hashMap[pos].contains(element)) {
+		data = temp;
+	}
+
+	/**
+	 * Diese Methode fügt einem uebergebenen HashSet ein neues Element hinzu.
+	 * @param element Das Element welches hinzugefuegt werden soll.
+	 * @param hashMap Die HashTabelle wo ein Element hinzuefuegt werden soll.
+	 * @return {@code true} Wenn das Element bereits existriert, sonst {@code false}
+	 */
+	private boolean addElement(K element, ArrayList<K>[] hashSet) {
+		int pos = hash(element, hashSet.length);
+
+		if(hashSet[pos] == null) {
+			hashSet[pos] = new ArrayList<>();
+		}
+
+		if(hashSet[pos].contains(element)) {
 			return true;
 		}
 
-		hashMap[pos].add(element);
+		hashSet[pos].add(element);
 		elements++;
 		return false;
 	}
 
 	/**
-	 * 
-	 * @param element
-	 * @param size
-	 * @return
+	 * Die Methode liefert den Hash-Wert für ein Element gemaess der definierten
+	 * HashFunktion welche die Groesse des Feldes benoetigt.
+	 * @param element Das Element von dem der Hash-Wert berechnet werden soll.
+	 * @param size Die Groesse der Hash-Tabelle.
+	 * @return Den ermittelten Wert als {@code int}.
 	 */
 	private int hash(K element, int size) {
 		return Math.abs(element.hashCode() % size);
+	}
+
+	/**
+	 * Diese Methode gibt ein Array mit dem richtigen Typ und der richtigen Groesse zurueck.
+	 * @param length Die Groesse des zu erzeugenden Arrays.
+	 * @return Ein {@code ArrayList<K>[]} mit der angegebenen Groesse.
+	 */
+	@SuppressWarnings("unchecked") // Java unterstruetzt leider immer noch keine Arrays für generische Typen.
+	private  ArrayList<K>[] createArray(int length) {
+		return (ArrayList<K>[]) new ArrayList[length];
 	}
 }
