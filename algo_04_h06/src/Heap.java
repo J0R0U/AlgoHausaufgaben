a6a9e2537bce6d6aa83c51fceabc2b7f2a011417
Jonas Rouven Schönauer <jonas.schoenauer@inform-software.com>
2019-04-30 16:00:15 +0200

    I refactored it later you know i refactored it later

--- algo_04_h06/src/Heap.java
+++ algo_04_h06/src/Heap.java
@@ -1,23 +1,53 @@
 import java.util.ArrayList;
 import java.util.Collections;
 
+/**
+ * Diese Klasse implemeniert einen Heap.
+ * 
+ * @author Maxime, Dominik, Jonas
+ * @version V01.01B00
+ */
 public class Heap {
+	/**
+	 * Der Wert des Dummyelements.
+	 */
+	private static int DUMMY_VALUE = 0;
+	
+	/**
+	 * Die Daten die in dem Heap gespeichert werden sollen.
+	 */
 	private ArrayList<Integer> data;
 	
+	/**
+	 * Der Konstruktor initialiesiert die Liste der daten und fuegt in Dummy an erster Stelle ein.
+	 */
 	public Heap() {
 		data = new ArrayList<>();
-		data.add(0);  // Dummy Element hinzufuegen
+		data.add(DUMMY_VALUE);
 	}
 	
+	/**
+	 * Prueft ob der Heap nur noch das Dummyelement enthaelt und somit leer ist.
+	 * @return {@code true}, wenn der Heap leer ist, sonst {@code false}
+	 */
 	public boolean isEmpty() {
 		return data.size() == 1;
 	}
 	
+	/**
+	 * Fuegt dem Heap ein neues Element hinzu.
+	 * @param i Das Element das hinzugefuegt werden sollt.
+	 */
 	public void add(int i) {
 		data.add(i);
 		upheap();
 	}
 	
+	/**
+	 * Loscht das groeﬂte Element und gibt dessen Wert zurueck.
+	 * @return Das vormals groeﬂte Element.
+	 * @throws Eine {@code IndexOutOfBoundsException} wenn der Heap leer ist.
+	 */
 	public int getMax() {
 		if(isEmpty()) {
 			throw new IndexOutOfBoundsException("Der Heap ist leer");
@@ -31,6 +61,8 @@ public class Heap {
 		return ret;
 	}
 	
+
+	@Override
 	public String toString() {
 		StringBuilder ret = new StringBuilder("[");
 		
@@ -45,6 +77,9 @@ public class Heap {
 		return ret.toString();
 	}
 	
+	/**
+	 * Diese Methode implementiert das upheap-Verfahren.
+	 */
 	private void upheap() {
 		int curr   = data.size() - 1;
 		int parent = curr / 2;
@@ -61,6 +96,9 @@ public class Heap {
 		}
 	}
 	
+	/**
+	 * Diese Methode implementiert das downheap-Verfahren.
+	 */
 	private void downheap() {
 		int curr      = 1;
 		int currChild = curr * 2;
--- algo_04_h06/src/TestHeap.java
+++ algo_04_h06/src/TestHeap.java
@@ -1,68 +1,34 @@
-
+/**
+ * Diese Klasse testet die Heap Klasse.
+ * 
+ * @author Maxime, Dominik, Jonas
+ * @version V01.01B00
+ */
 public class TestHeap {
+	/**
+	 * Die Zahlen die in den Heap hinzugefuegt werden sollen.
+	 */
 	private static final int[] TEST_NUMBERS       = {1, 6, 8, 18, 23, 5, 17, 20, 26, 21, 9};
-	private static final String[] EXPECTED_OUTPUT = {
-			"[]",
-			"[1]",
-			"[6, 1]",
-			"[8, 1, 6]",
-			"[18, 8, 6, 1]",
-			"[23, 18, 6, 1, 8]",
-			"[23, 18, 6, 1, 8, 5]",
-			"[23, 18, 17, 1, 8, 5, 6]",
-			"[23, 20, 17, 18, 8, 5, 6, 1]",
-			"[26, 23, 17, 20, 8, 5, 6, 1, 18]",
-			"[26, 23, 17, 20, 21, 5, 6, 1, 18, 8]",
-			"[26, 23, 17, 20, 21, 5, 6, 1, 18, 8, 9]",
-			"26",
-			"[23, 21, 17, 20, 9, 5, 6, 1, 18, 8]",
-			"23",
-			"[21, 20, 17, 18, 9, 5, 6, 1, 8]",
-			"21",
-			"[20, 18, 17, 8, 9, 5, 6, 1]",
-			"20",
-			"[18, 9, 17, 8, 1, 5, 6]",
-			"18",
-			"[17, 9, 6, 8, 1, 5]",
-			"17",
-			"[9, 8, 6, 5, 1]",
-			"9",
-			"[8, 5, 6, 1]",
-			"8",
-			"[6, 5, 1]",
-			"6",
-			"[5, 1]",
-			"5",
-			"[1]",
-			"1",
-			"[]"
-	};
 	
+	/**
+	 * Die main-Methode welche die Tests ausfuehrt.
+	 * @param args Die Kommandozeilenparameter.
+	 */
 	public static void main(String args[]) {
 		Heap h = new Heap();
 		
-		checkAndOutput(h);
+		System.out.println(h);
 		
 		for(int x : TEST_NUMBERS) {
 			h.add(x);
-			checkAndOutput(h);
+			System.out.println(h);
 		}
 		
 		System.out.println();
 		
 		while(!h.isEmpty()) {
-			checkAndOutput(h.getMax());
-			checkAndOutput(h);		}
-	}
-	
-	private static int posInStrings = 0;
-	private static void checkAndOutput(Object o) {
-		String s = o.toString();
-		if(!s.equals(EXPECTED_OUTPUT[posInStrings])) {
-			throw new RuntimeException(s + "!=" + EXPECTED_OUTPUT[posInStrings]);
+			System.out.println(h.getMax());
+			System.out.println(h);
 		}
-		System.out.println(s);
-		
-		posInStrings++;
 	}
 }
