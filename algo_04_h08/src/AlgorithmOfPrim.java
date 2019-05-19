98426d382072a36327c10816ef41f0406c449781
Jonas Rouven Schönauer <jonas.schoenauer@inform-software.com>
2019-05-19 17:06:56 +0200

    Added Comments

--- algo_04_h08/src/AlgorithmOfPrim.java
+++ algo_04_h08/src/AlgorithmOfPrim.java
@@ -1,17 +1,43 @@
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
+/**
+ * Diese Klasse implementiert den Algorithmus von Prim.
+ * 
+ * @author Maxime, Dominik, Jonas
+ * @version V01.01B00
+ */
 public class AlgorithmOfPrim {
+	/**
+	 * Diese Klasse representiert einen Knoten, der genutzt wird um den Algorithmus von Prim einfacher durchfuehren zu koennen.
+	 * 
+	 * @author Maxime, Dominik, Jonas
+	 * @version V01.01B00
+	 */
 	private static class PrimNode {
 		int nodeID;
 		int neighbourInMST;
 		int distanceFromMST;
 		
+		/**
+		 * Diese Methode prueft ob ein Objekt gleich einem andern ist.
+		 * @param o Das Objekt welches mit dem aktuellen verglichen werden soll.
+		 * @return {@code true} wenn o ein PrimNode ist und die IDs uebereinstimmen, sonst {@code false}
+		 */
 		public boolean equals(Object o) {
 			return o instanceof PrimNode && ((PrimNode)o).nodeID == nodeID;
 		}
 	}
 	
+	/**
+	 * Mithilfe dieser Methode wird eine PriorityQueue mit einer gewissen Groesse
+	 * initialiesiert. Alle Knoten sind dabei alle soweit wie moeglich von dem MST
+	 * entfernt, ausser der Startknoten dieser hat eine Entfernung -1, damit
+	 * dieser oben auf dem Heap landet.
+	 * @param size Die Groesse 
+	 * @param start Der Startknoten welcher die Wurzel des MST bildet.
+	 * @return Eine PriorityQueue welche initialiesiert wurde um den Algorithmus von Prim durchzufuehren.
+	 */
 	private static PriorityQueue<PrimNode> initQueue(int size, int start) {
 		PriorityQueue<PrimNode> queue = new PriorityQueue<PrimNode>(new Comparator<PrimNode>() {
 			public int compare(PrimNode o1, PrimNode o2) {
@@ -37,6 +63,11 @@ public class AlgorithmOfPrim {
 		return queue;
 	}
 	
+	/**
+	 * Diese Methode ermittelt einen MST aus einer gebenen Adjazenzmatrix mit dem Startknoten an Index 0.
+	 * @param edges Die Adjazenzmatrix welche die Kanten des Graphen enthaelt.
+	 * @return Die Kosten des MST.
+	 */
 	private static int getMST(int[][] edges) {		
 		PriorityQueue<PrimNode> queue = initQueue(edges.length, 0);
 		int ret = 0;
@@ -70,6 +101,10 @@ public class AlgorithmOfPrim {
 		return ret;
 	}
 	
+	/**
+	 * Die main-Methode welche die Tests ausfuehrt.
+	 * @param args Die Kommandozeilenparameter.
+	 */
 	public static void main(String[] args) {
 		int[][] adjazenzmatrix = { { 0, 3, 0, 2, 0, 0 },
 								   { 3, 0, 2, 0, 3, 0 },
